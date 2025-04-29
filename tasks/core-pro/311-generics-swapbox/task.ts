934898582edce8381a1cbc6cf62e09cf5ad16007
marek.cwirko <marek.cwirko@hotmail.com>
2025-04-29 07:28:40 +0200

    added generic function

--- tasks/core-pro/311-generics-swapbox/task.ts
+++ tasks/core-pro/311-generics-swapbox/task.ts
@@ -6,10 +6,12 @@ function swapStrings(a: string, b: string): [string, string] {
   return [b, a];
 }
 
-export function swapBox(a, b) {
-  throw new Error('Not implemented');
+export function swapBox<T>(a: T, b: T): [T, T] {
+  return [b, a];
 }
 
 const [n1, n2] = swapNumbers(10, 20);
 const [s1, s2] = swapStrings('hello', 'world');
 const [x, y] = swapBox(false, true);
+const [x1, y1] = swapBox(10, 20);
+const [x2, y2] = swapBox('hello', 'world');
