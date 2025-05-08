c86a0a19834c4c5f0dcc85419c570355355950dd
marek.cwirko <marek.cwirko@hotmail.com>
2025-05-08 23:10:40 +0200

    redefine  MergeableObject to apply Exclude from NonNullable

--- tasks/core-pro/331-generics-mergable/task.ts
+++ tasks/core-pro/331-generics-mergable/task.ts
@@ -1,5 +1,6 @@
 // Zaimplementuj typ MergeableObject z wykorzystaniem typów wbudowanych - Exclude i NonNullable.
-type MergeableObject<T> = T;
+type MergeableObject<T> = Exclude<NonNullable<T>, Function | any[] | string | number | boolean | symbol | bigint>;
+
 
 export function mergeObjects<T, U>(obj1: MergeableObject<T>, obj2: MergeableObject<U>): T & U {
   const merged = { ...obj1, ...obj2 };
